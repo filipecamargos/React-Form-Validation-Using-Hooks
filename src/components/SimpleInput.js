1aef4d18047eb9ee1479d09b2327c0a5a59d4794
filipecamargos <filipe.atrabalho@gmail.com>
2021-05-06 22:16:52 -0600

    moving with state and two way biding

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -9,13 +9,15 @@ const SimpleInput = (props) => {
 
   const formSubmitHandler = event => {
     event.preventDefault();
+
+    setEnteredName('');
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' onChange={nameInputChangeHandler}/>
+        <input type='text' id='name' value={enteredName} onChange={nameInputChangeHandler}/>
       </div>
       <div className="form-actions">
         <button>Submit</button>
